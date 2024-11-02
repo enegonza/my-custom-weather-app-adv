cc634d6e7bd88f053d3ec67fa1e594ed8aef973d
enegonza <vrtlly.eng@gmail.com>
2024-11-01 23:05:54 -0500

    for imperial units

--- src/script.js
+++ src/script.js
@@ -50,7 +50,7 @@ function updatedCity(response) {
   displayForecast(response.data.city);
 }
 function retrieveForecast(city) {
-  let forecastApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=e5d23o984ba0b21973288194ctbda24f`;
+  let forecastApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=e5d23o984ba0b21973288194ctbda24f&units=imperial`;
   axios.get(forecastApiUrl).then(displayForecast);
 }
 
